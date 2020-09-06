0859ae1551f43d620a68f2a1b4f94e61449ab6e5
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-06 15:26:23 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+    attr_accessor :name, :appointments, :patient
+    @@all = []
+    def initialize(name)
+        @name = name
+        @appointments = []
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+        end
+    end
+    def patients
+        appointments.map do |appointment|
+            appointment.patient
+        end
+    end
+
+    def new_appointment(date, patient)
+       appointment = Appointment.new(patient, date, self)
+       @appointments << appointment
+       appointment
+    end
+    #def new_appointment(patient, date)
+      #  Appointment.new(patient, date, self)
+    #end
+end
