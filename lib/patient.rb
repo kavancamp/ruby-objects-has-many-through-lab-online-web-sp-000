ccd3aff7b64f759b9bac2b2463395f5a45120fb2
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-06 15:39:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,22 +1,22 @@
 class Patient
   attr_accessor :name, :appointment, :doctor
     @@all = []
-    
+
     def initialize(name)
         @name = name
         @@all << self
     end
-    
+
     def self.all
         @@all
     end
-    
+
     def appointments
         Appointment.all.select do |appointment|
             appointment.patient == self
         end
     end
-    
+
     def new_appointment(doctor, date)
         Appointment.new(doctor, self, date)
     end
